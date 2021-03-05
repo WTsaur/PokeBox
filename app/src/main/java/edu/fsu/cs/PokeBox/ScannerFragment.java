a6a5ab9abb2d0fc26d5d6c7c6024f7f7544708f1
Tommy Chong <tc18cj@my.fsu.edu>
2021-03-05 16:11:03 -0500

    Add camera preview to scanner fragment

--- app/src/main/java/edu/fsu/cs/PokeBox/ScannerFragment.java
+++ app/src/main/java/edu/fsu/cs/PokeBox/ScannerFragment.java
@@ -5,19 +5,27 @@ import android.content.pm.PackageManager;
 import android.hardware.Camera;
 import android.media.Image;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.io.IOException;
+
+import static android.content.ContentValues.TAG;
+
 public class ScannerFragment extends Fragment {
 View view;
 private Camera camera;
-   // private CameraPreview mPreview;
+private CameraPreview mPreview;
 
     @Nullable
     @Override
@@ -26,7 +34,10 @@ private Camera camera;
                 R.layout.fragment_scanner, container, false);
         if(checkCameraHardware(getContext())) {
             camera = getCameraInstance();
-
+            // Create our Preview view and set it as the content of our activity.
+            mPreview = new CameraPreview(getContext(), camera);
+            FrameLayout preview = (FrameLayout) view.findViewById(R.id.camera);
+            preview.addView(mPreview);
 
             //setting the camera capture button
             ImageButton capture = (ImageButton) view.findViewById(R.id.capture);
@@ -59,5 +70,68 @@ private Camera camera;
             // Camera is not available (in use or does not exist)
         }
         return c; // returns null if camera is unavailable
+    }
+
+
+
+    /** A basic Camera preview class */
+    public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback {
+        private SurfaceHolder mHolder;
+        private Camera mCamera;
+
+        public CameraPreview(Context context, Camera camera) {
+            super(context);
+            mCamera = camera;
+
+            // Install a SurfaceHolder.Callback so we get notified when the
+            // underlying surface is created and destroyed.
+            mHolder = getHolder();
+            mHolder.addCallback(this);
+            // deprecated setting, but required on Android versions prior to 3.0
+            mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+        }
+
+        public void surfaceCreated(SurfaceHolder holder) {
+            // The Surface has been created, now tell the camera where to draw the preview.
+            try {
+                mCamera.setPreviewDisplay(holder);
+                mCamera.startPreview();
+            } catch (IOException e) {
+                Log.d(TAG, "Error setting camera preview: " + e.getMessage());
+            }
+        }
+
+        public void surfaceDestroyed(SurfaceHolder holder) {
+            // empty. Take care of releasing the Camera preview in your activity.
+        }
+
+        public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
+            // If your preview can change or rotate, take care of those events here.
+            // Make sure to stop the preview before resizing or reformatting it.
+
+            if (mHolder.getSurface() == null){
+                // preview surface does not exist
+                return;
+            }
+
+            // stop preview before making changes
+            try {
+                mCamera.stopPreview();
+            } catch (Exception e){
+                // ignore: tried to stop a non-existent preview
+            }
+
+            // set preview size and make any resize, rotate or
+            // reformatting changes here
+
+            // start preview with new settings
+            try {
+                mCamera.setPreviewDisplay(mHolder);
+                mCamera.startPreview();
+
+            } catch (Exception e){
+                Log.d(TAG, "Error starting camera preview: " + e.getMessage());
+            }
+        }
     }
 }
